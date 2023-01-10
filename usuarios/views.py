d3468441c47c5bf212a3bd291a45d5db28888ad2
Flavio Macedo <fhenrique609@gmail.com>
2023-01-09 22:39:28 -0300

    Adicionando as mensagem de alerta

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.messages import constants
 
 
 def cadastro(request):
@@ -12,9 +14,11 @@ def cadastro(request):
         confirmar_senha = request.POST.get('confirmar_senha')
 
         if len(nome.strip()) == 0 or len(email.strip()) == 0 or len(senha.strip()) == 0 or len(confirmar_senha.strip()) == 0:
+            messages.add_message(request, constants.ERROR, 'Preencha todos os campos.')
             return render(request, 'cadastro.html')
 
         if senha != confirmar_senha:
+            messages.add_message(request, constants.ERROR, 'Digite duas senhas iguais.')
             return render(request, 'cadastro.html')
 
         try:
@@ -23,8 +27,8 @@ def cadastro(request):
                 email=email,
                 password=senha,
             )
-            # Mensagem de Sucesso
+            messages.add_message(request, constants.SUCCESS, 'Usuário criado com sucesso.')
             return render(request, 'cadastro.html')
         except:
-            # Mensagem de erro
+            messages.add_message(request, constants.ERROR, 'Erro interno do sistema.')
             return render(request, 'cadastro.html')
